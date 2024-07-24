430a5eae07242fa10609c2ffbaccc3e192ecd056
Hassan-Munawar <hassanbuiness@gmail.com>
2024-07-25 00:32:02 +0500

    Added index.html

---
+++ main.js
@@ -0,0 +1,19 @@
+var typed= new Typed(".text",{
+    strings:["Frontend Developer" , "Video Editor" , "Thumbnail Designer"],
+   typeSpeed: 100,
+   backSpeed: 100,
+  backDelay: 100,
+  loop: true
+});
+
+
+// for Downloading resume;
+document.getElementById('downloadButton').addEventListener('click', function() {
+  var link = document.createElement('a');
+  link.href = 'resume.pdf';
+  link.download = 'Resume.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+});
+
